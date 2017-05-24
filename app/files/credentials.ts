208370973112f08a85c64eea06c695f471b6f1fc
NIkolay Iliev <NIkolay Iliev>
2017-05-24 20:08:05 +0300

    add

--- app/files/credentials.ts
+++
@@ -1,3 +0,0 @@
-export const API_KEY = "?ts=1&apikey=ba6b3f50e03ce86e096a4de5a89f18eb&hash=209d0cef2e3cdb793c6770e701f87566";
-export const COMICS_URL = "https://gateway.marvel.com/v1/public/comics";
-export const LIMIT = "&limit=100";
